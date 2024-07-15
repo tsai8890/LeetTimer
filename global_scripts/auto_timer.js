f3eca7f472c9fe61271811fea83813c4eb0c3a10
tsai8890 <asdpeter6520@gmail.com>
2024-07-15 11:08:24 +0800

    fix: set timeout to refresh start button

--- global_scripts/auto_timer.js
+++ global_scripts/auto_timer.js
@@ -2,6 +2,12 @@ var timer_started = null;
 var first_entry = true;
 
 
+// Web Interface on LeetCode
+var start_button = null;
+var reset_button = null;
+var show_timer_button = null;
+
+
 window.onload = () => {
     var observer = new MutationObserver(function(mutations) {
         timer = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.flex.items-center.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary");
@@ -9,6 +15,11 @@ window.onload = () => {
             timer_icon = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.flex.items-center.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary > div.flex.flex-none.py-2.pl-2.pr-1.text-gray-60.dark\\:text-gray-60 > div > svg");
             timer_started = (timer_icon.getAttribute('data-icon') === 'circle-pause');
         }
+        start_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.flex.items-center.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary");
+        reset_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.rounded-\\[3px\\].p-2.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary.text-gray-60.dark\\:text-gray-60");
+        show_timer_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(2) > div");
+        
+
     });
     observer.observe(document, {attributes: false, childList: true, characterData: false, subtree: true});
 }
@@ -17,21 +28,25 @@ window.onload = () => {
 document.addEventListener('keypress', async (e) => {
     if (timer_started !== null) {
         try {
-            const show_timer_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(2) > div");
             show_timer_button.click();
         }
         catch (e) { }
         finally {
-            if (first_entry) {
-                const reset_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.rounded-\\[3px\\].p-2.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary.text-gray-60.dark\\:text-gray-60");
-                reset_button.click();
+            if (first_entry === true) {
                 first_entry = false;
-            }
 
-            if (!timer_started) {
-                const start_button = document.querySelector("#ide-top-btns > div.relative.flex > div.relative.flex.overflow-hidden.rounded.bg-fill-tertiary.dark\\:bg-fill-tertiary.mr-\\[6px\\] > div > div:nth-child(1) > div > div.flex.items-center.hover\\:bg-fill-quaternary.dark\\:hover\\:bg-fill-quaternary");
+                if (timer_started === false) {
+                    start_button.click();
+                
+                } else {
+                    reset_button.click();
+                    setTimeout(() => {
+                        start_button.click(); 
+                    }, 500);
+                }
+            } else if (timer_started === false) {
                 start_button.click();
             }
         }
-    }   
+    }
 })
